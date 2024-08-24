076957bbb754b955d7ee5904327443874983767c
Al-3x <166305327+Al-3x@users.noreply.github.com>
2024-08-24 20:51:12 -0300

    Update FirstActivity.kt

--- JogodaMatematica/app/src/main/java/com/example/jogodamatematica/FirstActivity.kt
+++ JogodaMatematica/app/src/main/java/com/example/jogodamatematica/FirstActivity.kt
@@ -7,26 +7,32 @@ import com.example.jogodamatematica.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
 
-    // Definindo a variável de binding
+    // Definindo a variável de binding para acessar as views da interface do usuário
     private lateinit var binding: ActivityStartBinding
 
+    // Método onCreate chamado quando a atividade é criada pela primeira vez
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Inflando o layout usando View Binding
+        
+        // Inflando o layout associado com o binding, permitindo o acesso às views sem findViewById
         binding = ActivityStartBinding.inflate(layoutInflater)
+        
+        // Define o conteúdo da atividade para o layout raiz da vinculação
         setContentView(binding.root)
 
-        // Configurando o listener de clique do botão
+        // Configurando o listener de clique para o botão "Iniciar Jogo"
         binding.startGameButton.setOnClickListener {
-            // Ação ao clicar no botão "Iniciar Jogo"
+            // Chama o método para abrir a MainActivity ao clicar no botão
             openMainActivity()
         }
     }
 
+    // Método para iniciar a MainActivity
     private fun openMainActivity() {
-        // Iniciar a MainActivity
+        // Cria uma intent para iniciar a MainActivity
         val intent = Intent(this, MainActivity::class.java)
+        
+        // Inicia a MainActivity usando a intent criada
         startActivity(intent)
     }
 }
